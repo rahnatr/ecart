5f97293d56594b9053af8d1a7c41159cca9b452b
rahnatr <rahnatcr@gmail.com>
2024-02-02 16:36:26 +0530

    add index page and load static in style sheet

--- ecart/urls.py
+++ ecart/urls.py
@@ -14,14 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import statistics
+
 from django.conf import settings
-from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf.urls.static import static
+from django.contrib import admin
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/',admin.site.urls),
+    path('',include('products.urls'))
+    
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
---
+++ products/urls.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.urls import include, path
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    path('',views.index),
+   
+]
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
